ae1cdb10559fee760e6c1d88df15185aafd18115
Owain Williams <owain@andersdigital.com>
2023-06-09 11:56:15 +0100

    ui: improve nav

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,34 +1,44 @@
+"use client";
 import { UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
-
-const navItems = [
-  {
-    name: "Write",
-    href: "/write",
-  },
-  {
-    name: "Home",
-    href: "/",
-  },
-];
+import { Button } from "@/components/ui/button";
+import { usePathname } from "next/navigation";
 
 export default async function NavBar() {
+  const pathname = usePathname();
+  const navItems = [
+    {
+      name: "Write",
+      href: "/write",
+      isActive: pathname === "/write",
+    },
+    {
+      name: "Home",
+      href: "/",
+      isActive: pathname === "/",
+    },
+  ];
+
   return (
     <nav className="flex flex-row h-20 min-w-full border border-x-0 border-t-0 border-slate-200 drop-shadow-md">
       <div className="flex flex-row items-center justify-end w-full px-4">
-        <div className="pr-8">
-          {navItems.map((item) => (
-            <Link href={item.href} key={item.name} className="px-4">
-              {item.name}
-            </Link>
-          ))}
-        </div>
         <SignedIn>
+          <div className="pr-8">
+            {navItems.map((item) => (
+              <Link href={item.href} key={item.name} className={`px-4`}>
+                <Button disabled={item.isActive} variant="link">
+                  {item.name}
+                </Button>
+              </Link>
+            ))}
+          </div>
           <UserButton />
         </SignedIn>
         <SignedOut>
-          <SignInButton />
+          <Button asChild>
+            <SignInButton />
+          </Button>
         </SignedOut>
       </div>
     </nav>
