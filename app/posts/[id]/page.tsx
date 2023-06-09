1ebdee8b6724cf5ffa423e35202ef4be1339e81d
Owain Williams <owain@andersdigital.com>
2023-06-09 14:51:19 +0100

    ui: beautify post page

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -18,14 +18,16 @@ export default async function PostPage({ params }: { params: { id: string } }) {
   const author = await clerkClient.users.getUser(post.authorId);
 
   return (
-    <div className="px-2 py-4 md:px-24 md:py-8 xl:px-48 xl:py-12">
-      <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
-        {post.title}
-      </h2>
-      <sub className="flex text-sm text-muted-foreground">{`by ${
-        author.firstName
-      } ${author.lastName} - ${post.createdAt.toUTCString()}`}</sub>
-      <p className="leading-7 [&:not(:first-child)]:mt-6">{post.content}</p>
+    <div className="flex flex-col items-center h-[calc(100vh-4rem)] px-0 md:px-24 py-8 bg-gradient-to-r from-indigo-50 from-10% via-sky-50 via-30% to-emerald-50 to-90%">
+      <div className="flex flex-col bg-white p-8 rounded-md shadow-md">
+        <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+          {post.title}
+        </h2>
+        <sub className="flex text-sm text-muted-foreground">{`by ${
+          author.firstName
+        } ${author.lastName} - ${post.createdAt.toUTCString()}`}</sub>
+        <p className="leading-7 [&:not(:first-child)]:mt-6">{post.content}</p>
+      </div>
     </div>
   );
 }
